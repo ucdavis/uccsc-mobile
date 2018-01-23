e7a90a0c96c57984ccc1785edc2634409fc3851f
John Knoll <jpknoll@gmail.com>
2018-01-23 14:13:52 -0800

    add notifications redux
    has debug starter notification

---
+++ Redux/NotificationRedux.js
@@ -0,0 +1,32 @@
+import { createReducer, createActions } from 'reduxsauce'
+import Immutable from 'seamless-immutable'
+
+/* ------------- Initial State ------------- */
+export const INITIAL_STATE = Immutable({
+  notifications: ["This is my new message"],
+})
+
+/* ------------- Types and Action Creators ------------- */
+
+const { Types, Creators } = createActions({
+  addNotification: ['message'],
+  clearNotifications: null,
+})
+
+export const NotificationTypes = Types
+export default Creators
+
+/* ------------- Reducer ------------- */
+
+export const addSingleNotification = (state = INITIAL_STATE, { message }) =>
+  state.merge({ notifications: [...state.notifications, message] })
+
+export const clearAllNotifications = (state) =>
+  state.merge({notifications: []})
+
+/* ------------- Hookup Reducers To Types ------------- */
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.ADD_NOTIFICATION]: addSingleNotification,
+  [Types.CLEAR_NOTIFICATIONS]: clearAllNotifications,
+})
