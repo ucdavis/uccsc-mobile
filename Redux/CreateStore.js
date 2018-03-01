b751f8640d5a61eaa1092fff2b383ba26294e60b
John Knoll <jpknoll@gmail.com>
2018-03-01 12:12:32 -0800

    style

--- Redux/CreateStore.js
+++ Redux/CreateStore.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import Config from '../Config/DebugConfig';
 // import createSagaMiddleware from 'redux-saga'
-import RehydrationServices from '../Services/RehydrationServices';
 import ReduxPersistConfig from '../Config/ReduxPersistConfig';
 // import ScreenTracking from './ScreenTrackingMiddleware'
 
@@ -27,18 +26,24 @@ export default (rootReducer, rootSaga) => {
   enhancers.push(applyMiddleware(...middleware));
 
   // if Reactotron is enabled (default for __DEV__), we'll create the store through Reactotron
-  const createAppropriateStore = Config.useReactotron ? console.tron.createStore : createStore;
+  const createAppropriateStore = Config.useReactotron
+    ? console.tron.createStore
+    : createStore;
 
   // if Persist is enabled, wrap the root reducer
-  const persistedRootReducer = ReduxPersistConfig.active ? persistReducer(ReduxPersistConfig.storeConfig, rootReducer) : rootReducer;
+  const persistedRootReducer = ReduxPersistConfig.active
+    ? persistReducer(ReduxPersistConfig.storeConfig, rootReducer)
+    : rootReducer;
 
   const store = createAppropriateStore(persistedRootReducer, compose(...enhancers));
 
   // configure persistStore and check reducer version number
-  const persistor = ReduxPersistConfig.active ? persistStore(store) : null;
+  const persistor = ReduxPersistConfig.active
+    ? persistStore(store)
+    : null;
 
   // kick off root saga
   // sagaMiddleware.run(rootSaga)
 
   return { store, persistor };
-}
+};
