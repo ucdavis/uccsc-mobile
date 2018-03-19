bcd74f3cbcfd8ca93b82eaa2a0c54013a2879925
John Knoll <jpknoll@gmail.com>
2018-03-19 15:17:32 -0700

    push transformation to cloud function

--- Services/Api.js
+++ Services/Api.js
@@ -1,48 +1,13 @@
-import { parse } from '../Utils/JsonApi';
 
 export async function getSchedule() {
-  const fields = [
-    // 'type',
-    // 'revision_uid',
-    // 'uid',
-    'field_event_sponsors',
-    'field_event_venue',
-    'field_session_files',
-    'field_session_length',
-    'field_session_skill_level',
-    'field_session_speakers',
-    'field_session_speakers.field_uccsc_user_photo',
-    'field_session_track',
-    'field_session_type',
-  ].join(',');
 
   // url
-  const url = `https://test-uccsc.pantheonsite.io/jsonapi/node/session?include=${fields}`;
+  const url = 'https://us-central1-uccsc-ac393.cloudfunctions.net/getAllSessions';
 
   // fetch sessions
   const response = await fetch(url);
 
-  const result = parse(await response.text());
-
-  const sessions = result.data.map(d => {
-    const speakers = d.field_session_speakers.map(s => {
-      return {
-        name: s.name,
-      };
-    });
-
-    const session = {
-      type: 'talk',
-      speaker: speakers[0].name,
-      title: d.title,
-      description: d.field_session_description.value,
-      speakerInfo: speakers[0],
-      time: '7/10/2017 8:45 AM',
-      duration: d.field_session_length.name,
-    };
-
-    return session;
-  });
+  const sessions = JSON.parse(await response.text());
 
   return sessions;
 }
