0e7f45a95c2cc5b524778d20a0a1dacced5431e9
John Knoll <jpknoll@gmail.com>
2018-03-19 15:17:18 -0700

    style

--- Components/TalkInfo.js
+++ Components/TalkInfo.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import styles from './Styles/TalkInfoStyle'
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './Styles/TalkInfoStyle';
 
 const TalkInfo = (props) => {
-    const { start, duration, remindMe, toggleRemindMe, isFinished, showWhenFinished } = props
-  
-    return (
-      <View style={styles.container}>
-        <View style={styles.details}>
-          <View style={styles.detail}>
-            <Text style={styles.detailLabel}>
-              Start
-            </Text>
-            <Text style={styles.detailText}>
-            </Text>
-          </View>
-          <View style={styles.detail}>
-            <Text style={styles.detailLabel}>
-              Duration
-            </Text>
-            <Text style={styles.detailText}>
-              {`${duration} Minutes`}
-            </Text>
-          </View>
+  const { start, duration, remindMe, toggleRemindMe, isFinished, showWhenFinished } = props;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.details}>
+        <View style={styles.detail}>
+          <Text style={styles.detailLabel}>
+            Start
+          </Text>
+          <Text style={styles.detailText} />
+        </View>
+        <View style={styles.detail}>
+          <Text style={styles.detailLabel}>
+            Duration
+          </Text>
+          <Text style={styles.detailText}>
+            {duration}
+          </Text>
         </View>
       </View>
-    )
-  }
-  export default TalkInfo
+    </View>
+  );
+};
+
+export default TalkInfo;
