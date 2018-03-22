a08ee35d1c5ccc29ac2b6eb2c9bf8717c5854161
John Knoll <jpknoll@gmail.com>
2018-03-22 12:03:14 -0700

    Fill out component

--- Components/AboutScreen.js
+++ Components/AboutScreen.js
@@ -1,26 +1,100 @@
-import React from 'react'
+import React from 'react';
 import {
+  ScrollView,
+  TouchableOpacity,
   Image,
   Text,
   View,
-} from 'react-native'
+  LayoutAnimation,
+} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import { Images } from '../Themes'
-import styles from './Styles/AboutScreenStyle'
+import Gradient from '../Components/Gradient';
+import SeeProcess from '../Components/SeeProcess';
+import Twitter from '../Components/Twitter';
+import Sponsors from '../Components/Sponsors';
+import LiveHelp from '../Components/LiveHelp';
+import ConferenceAnnouncements from '../Components/ConferenceAnnouncements';
+import { Images } from '../Themes';
+import styles from './Styles/AboutScreenStyle';
 
 export default class AboutScreen extends React.Component {
   static navigationOptions = {
     tabBarLabel: 'General Info',
     tabBarIcon: ({ focused }) => (
-      <MaterialIcons name="info" size={24} color="black" />
-    )
+      <MaterialIcons name="info" size={24} color="white" />
+    ),
   }
 
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTab: 'liveHelp',
+    };
+  }
+
+  setActiveTab = (tab) => {
+    LayoutAnimation.configureNext({ ...LayoutAnimation.Presets.linear, duration: 250 });
+    this.setState({ activeTab: tab });
+  }
+
+  renderTabs() {
+    const { activeTab } = this.state;
+    const liveHelpStyles = [
+      styles.tab,
+      activeTab === 'liveHelp' && styles.activeTab,
+    ];
+    const sponsorStyles = [
+      styles.tab,
+      activeTab === 'sponsors' && styles.activeTab,
+    ];
+    const liveHelpTextStyles = [
+      styles.tabText,
+      activeTab === 'liveHelp' && styles.activeTabText,
+    ];
+    const sponsorTextStyles = [
+      styles.tabText,
+      activeTab === 'sponsors' && styles.activeTabText,
+    ];
+
     return (
-        <View style={styles.container}>
-          <Text style={styles.sectionText}>About Screen</Text>
+      <View style={styles.tabsContainer}>
+        <View style={styles.tabs}>
+          <TouchableOpacity
+            style={liveHelpStyles}
+            onPress={() => this.setActiveTab('liveHelp')}>
+            <Text style={liveHelpTextStyles}>
+              Live Help
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={sponsorStyles}
+            onPress={() => this.setActiveTab('sponsors')}>
+            <Text style={sponsorTextStyles}>
+              Sponsors
+            </Text>
+          </TouchableOpacity>
         </View>
-      )
+        {this.renderTabsContent()}
+      </View>
+    );
+  }
+
+  renderTabsContent() {
+    const { activeTab } = this.state;
+    return activeTab === 'liveHelp' ? <LiveHelp /> : <Sponsors />;
+  }
+
+  render() {
+    return (
+      <Gradient style={styles.container}>
+        <ScrollView>
+          <ConferenceAnnouncements currentDate={this.props.currentTime} />
+          <Twitter />
+          <SeeProcess />
+          {this.renderTabs()}
+        </ScrollView>
+      </Gradient>
+    );
   }
 }
