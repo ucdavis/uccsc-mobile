303e977fb565d73d10744914256cc48c96f5f53c
John Knoll <jpknoll@gmail.com>
2018-03-22 12:03:25 -0700

    New UCD Colors

--- Themes/Colors.js
+++ Themes/Colors.js
@@ -1,4 +1,4 @@
-import {Platform} from 'react-native'
+import { Platform } from 'react-native';
 
 const colors = {
   background: '#F4F4F4',
@@ -22,13 +22,36 @@ const colors = {
   border: '#483F53',
   banner: '#5F3E63',
 
-  blue: '#018EAA',
-  darkBlue1: '#008099',
+  // blue: '#018EAA',
+  // darkBlue1: '#008099',
 
   green: '#78BE20',
   darkGreen1: '#6CAB1C',
 
   darkPurple1: '#7D4A80',
+
+  // UC Davis Color Palette
+  gold: '#D2AC3A',
+  blue: '#004987',
+  darkBlue1: '#002855',
+
+  unitransRed: '#ba0c2f',
+  westernRedbud: '#c6007e',
+  californiaPoppy: '#ed8800',
+  goldenLupine: '#ffcd00',
+  sunnyGrass: '#78be20',
+  skyBlue: '#00b5e2',
+  recpoolBlue: '#008eaa',
+  wineGrape: '#642667',
+  // Supplemental neutral color palette for UC Davis
+  muBrick: '#c26e60',
+  harthallStucco: '#e6a65d',
+  sageGreen: '#9caf88',
+  evergreen: '#00573f',
+  winterskyGray: '#5b7f95',
+  cenwalkGray: '#b1b3b3',
+  corkOak: '#aca39a',
+  shingleBrown: '#4f2c1d',
 
   snow: 'white',
   red: '#E64044',
@@ -42,7 +65,7 @@ const colors = {
   avatarBorder: '#DCE3E8',
   lightText: '#656565',
   text: '#000000',
-  transparentBump: (Platform.OS === 'ios') ? 'rgba(140,42,140, 0.5)' : 'rgba(140,42,140, 0.9)'
-}
+  transparentBump: (Platform.OS === 'ios') ? 'rgba(140,42,140, 0.5)' : 'rgba(140,42,140, 0.9)',
+};
 
-export default colors
+export default colors;
