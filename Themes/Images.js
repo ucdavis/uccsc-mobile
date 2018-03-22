c816e5fbf27533abeb79b1ab3192bfdc662bc2ae
John Knoll <jpknoll@gmail.com>
2018-03-22 12:04:03 -0700

    Some new images

--- Themes/Images.js
+++ Themes/Images.js
@@ -1,7 +1,8 @@
 const images = {
   Arboretum: require('../Images/arboretum_landscape_4904.jpg'),
-  HeaderGradient: require('../Images/gradient.png'),
   LogoWithText: require('../Images/logo_with_text.png'),
+  HeaderGradient: require('../Images/gradient.png'),
+  GradientVertical: require('../Images/gradient-vertical.png'),
 };
 
 export default images;
