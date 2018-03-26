b3459bfa884e11e181945774506490922f11f78f
John Knoll <jpknoll@gmail.com>
2018-03-26 13:59:06 -0700

    code style

--- Themes/Fonts.js
+++ Themes/Fonts.js
@@ -2,42 +2,42 @@ const type = {
   base: 'Montserrat-Light',
   bold: 'Montserrat-SemiBold',
   semiBold: 'Montserrat-SemiBold',
-  medium: 'Montserrat-Medium'
-}
+  medium: 'Montserrat-Medium',
+};
 
 const size = {
   h1: 38,
   h2: 34,
   h5: 20,
   regular: 17,
-  medium: 14
+  medium: 14,
 }
 
 const style = {
   h1: {
     fontFamily: type.base,
-    fontSize: size.h1
+    fontSize: size.h1,
   },
   h2: {
     fontWeight: 'bold',
-    fontSize: size.h2
+    fontSize: size.h2,
   },
   h5: {
     fontFamily: type.base,
-    fontSize: size.h5
+    fontSize: size.h5,
   },
   normal: {
     fontFamily: type.base,
-    fontSize: size.regular
+    fontSize: size.regular,
   },
   description: {
     fontFamily: type.base,
-    fontSize: size.medium
-  }
-}
+    fontSize: size.medium,
+  },
+};
 
 export default {
   type,
   size,
-  style
-}
+  style,
+};
