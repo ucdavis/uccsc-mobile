30677ffe279c979b17a8099372fe0f59c2c3f0f1
John Knoll <jpknoll@gmail.com>
2018-05-24 14:02:17 -0700

    update conference dates

--- Config/AppConfig.js
+++ Config/AppConfig.js
@@ -2,6 +2,6 @@ export default {
   // font scaling override - RN default is on
   allowTextFontScaling: false,
   // Dates of the conference
-  conferenceDates: ['7/10/2017', '7/11/2017', '7/12/2017'],
-  conferenceUrl: 'https://test-uccsc.pantheonsite.io',
+  conferenceDates: ['8/13/2018', '8/14/2018', '8/15/2018'],
+  conferenceUrl: 'https://uccsc.ucdavis.edu',
 };
