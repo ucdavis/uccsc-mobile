81cdfa8ecddcbe628a15b368c8fa07a498903ebf
John Knoll <jpknoll@gmail.com>
2018-05-29 15:49:56 -0700

    Fix header format

--- Components/ScheduleSectionHeader.js
+++ Components/ScheduleSectionHeader.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './Styles/ScheduleSectionHeaderStyle';
+import { format } from 'date-fns';
 
 export default class ScheduleSectionHeader extends React.PureComponent {
   render() {
     const { time } = this.props;
+    const header = format(time, "h:mm A");
     return (
       <View style={styles.sectionHeaderContainer}>
-        <Text style={styles.headerTime}>{time}</Text>
+        <Text style={styles.headerTime}>{ header }</Text>
       </View>
     );
   }
