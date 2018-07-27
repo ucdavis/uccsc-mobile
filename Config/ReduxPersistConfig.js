fb75c368756ed7afb76623cd4a62d5bcde963eaf
John Knoll <jpknoll@gmail.com>
2018-07-27 12:18:45 -0700

    don't blacklist

--- Config/ReduxPersistConfig.js
+++ Config/ReduxPersistConfig.js
@@ -7,7 +7,7 @@ export default {
     key: 'root',
     storage: AsyncStorage,
     // reducer keys that you do NOT want stored to persistence here
-    blacklist: ['login', 'search', 'nav', 'schedule', 'location'],
+    // blacklist: [],
     // whitelist: [], Optionally, just specify the keys you DO want stored to
     // persistence. An empty array means 'don't store any reducers' -> infinitered/ignite#409
     // transforms: [],
