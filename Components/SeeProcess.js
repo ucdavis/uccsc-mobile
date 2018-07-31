99eef552ba68157fd759147df925ba0e92fd0a75
John Knoll <jpknoll@gmail.com>
2018-07-31 10:19:51 -0700

    link to talk

--- Components/SeeProcess.js
+++ Components/SeeProcess.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import { Text, TouchableOpacity, Linking } from 'react-native';
+import { connect } from 'react-redux';
+import { StackActions } from 'react-navigation';
 import { FontAwesome } from '@expo/vector-icons';
+
+import ScheduleActions from '../Redux/ScheduleRedux';
+import * as NavigationService from '../Services/NavigationService';
+
 import styles from './Styles/SeeProcessStyle';
 
-const onPress = () => {
-  const url = 'https://shift.infinite.red/making-the-react-native-conference-app-in-react-native-9afd36be3681';
-  Linking.openURL(url);
+class SeeProcess extends React.Component {
+  onPress = () => {
+    const { setSelectedEvent, talk } = this.props;
+    
+    setSelectedEvent(talk);
+    NavigationService.navigate({ routeName: 'Schedule', action: StackActions.push({ routeName: 'TalkDetail' }) });
+  }
+
+  render() {
+    return (
+      <TouchableOpacity style={styles.processContainer} onPress={this.onPress}>
+        <FontAwesome name="code-fork" size={24} color="white" style={styles.codeIcon} />
+        <Text style={styles.text}>See the process behind making our app</Text>
+        <FontAwesome name="long-arrow-right" size={24} color="white" style={styles.rightArrow} />
+      </TouchableOpacity>
+    );
+  }
+}
+
+const mapStateToProps = (store) => {
+  const talk = store.schedule.talks.find(t => t.title === 'Creating the UCCSC 2018 Mobile App');
+  return {
+    talk
+  };
 };
 
-const SeeProcess = (props) => {
-  return (
-    <TouchableOpacity style={styles.processContainer} onPress={onPress}>
-      <FontAwesome name="code-fork" size={24} color="white" style={styles.codeIcon} />
-      <Text style={styles.text}>See the process behind making our app</Text>
-      <FontAwesome name="long-arrow-right" size={24} color="white" style={styles.rightArrow} />
-    </TouchableOpacity>
-  );
-};
+// wraps dispatch to create nicer functions to call within our component
+const mapDispatchToProps = (dispatch) => ({
+  setSelectedEvent: (talk) => dispatch(ScheduleActions.setSelectedEvent(talk)),
+});
 
-export default SeeProcess;
+export default connect(mapStateToProps, mapDispatchToProps)(SeeProcess);
