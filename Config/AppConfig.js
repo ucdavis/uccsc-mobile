350c1b364af917a80d4fa3077bd909681dbd8695
John Knoll <jpknoll@gmail.com>
2018-08-07 11:39:54 -0700

    disable animations config

--- Config/AppConfig.js
+++ Config/AppConfig.js
@@ -1,7 +1,10 @@
+import { AccessibilityInfo } from 'react-native';
+
 export default {
   // font scaling override - RN default is on
   allowTextFontScaling: false,
   // Dates of the conference
   conferenceDates: ['8/13/2018', '8/14/2018', '8/15/2018'],
   conferenceUrl: 'https://uccsc.ucdavis.edu',
+  disableAnimations: AccessibilityInfo.fetch(),
 };
