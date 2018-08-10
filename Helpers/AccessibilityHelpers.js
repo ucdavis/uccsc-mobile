5bccc65064040b6f3528f6101c65920a323f6455
John Knoll <jpknoll@gmail.com>
2018-08-10 15:09:19 -0700

    wrap native code in try/catch
    probably not going to help all that much...

--- Helpers/AccessibilityHelpers.js
+++ Helpers/AccessibilityHelpers.js
@@ -4,12 +4,18 @@ import {
   Platform,
   UIManager,
 } from 'react-native';
+import Sentry from 'sentry-expo';
 
 export function accessibilityFocusRef(ref) {
-  const nodeHandle = findNodeHandle(ref);
-  if (Platform.OS === 'ios') {
-    AccessibilityInfo.setAccessibilityFocus(nodeHandle);
-  } else {
-    UIManager.sendAccessibilityEvent(nodeHandle, 8);
+  try {
+    const nodeHandle = findNodeHandle(ref);
+    if (Platform.OS === 'ios') {
+      AccessibilityInfo.setAccessibilityFocus(nodeHandle);
+    } else {
+      UIManager.sendAccessibilityEvent(nodeHandle, 8);
+    }
+  }
+  catch (err) {
+    Sentry.captureException(err);
   }
 }
--- Services/PushNotifications.js
+++ Services/PushNotifications.js
@@ -1,5 +1,6 @@
 import { Platform } from 'react-native';
 import { Permissions, Notifications } from 'expo';
+import Sentry from 'sentry-expo';
 import { isPast } from 'date-fns';
 
 import Config from '../Config/ServerConfig';
@@ -8,21 +9,26 @@ import PNHelpers from '../Helpers/PushNotificationHelpers';
 const presetNotifications = require('../Fixtures/notifications.json');
 
 export async function checkNotificationPermission() {
-  const { status: existingStatus } = await Permissions.getAsync(
-    Permissions.NOTIFICATIONS
-  );
-  let finalStatus = existingStatus;
+  try {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
 
-  // only ask if permissions have not already been determined, because
-  // iOS won't necessarily prompt the user a second time.
-  if (existingStatus !== 'granted') {
-    // Android remote notification permissions are granted during the app
-    // install, so this will only ask on iOS
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    finalStatus = status;
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    return finalStatus;
+  } catch (err) {
+    Sentry.captureException(err);
+    return "denied";
   }
-
-  return finalStatus;
 }
 
 export async function registerForPushNotificationsAsync() {
@@ -37,8 +43,8 @@ export async function registerForPushNotificationsAsync() {
   let token;
   try {
     token = await Notifications.getExpoPushTokenAsync();
-  } catch (err){
-    console.error(err);
+  } catch (err) {
+    Sentry.captureException(err);
   }
 
   if (!token) {
@@ -86,56 +92,72 @@ export async function scheduleTalkReminder(talk) {
 }
 
 export async function schedulePresetNotifications() {
-  for (var i = 0; i < presetNotifications.length; i++) {
-    const notification = presetNotifications[i];
-    if (!notification.enabled) {
-      return;
-    }
+  try {
+    for (var i = 0; i < presetNotifications.length; i++) {
+      const notification = presetNotifications[i];
+      if (!notification.enabled) {
+        return;
+      }
 
-    await scheduleNotification({
-      message: notification.title,
-      link: notification.data.link,
-      time: notification.time,
-    });
+      await scheduleNotification({
+        message: notification.title,
+        link: notification.data.link,
+        time: notification.time,
+      });
+    }
+  }
+  catch (err) {
+    Sentry.captureException(err);
   }
 }
 
 export async function scheduleNotification(notification) {
+  try {
+    // Stop here if the user did not grant permissions
+    const permission = await checkNotificationPermission();
+    if (permission !== 'granted') {
+      return 0;
+    }
 
-  // Stop here if the user did not grant permissions
-  const permission = await checkNotificationPermission();
-  if (permission !== 'granted') {
-    return 0;
+    const time = PNHelpers.notificationTime(notification.time);
+    if (isPast(time)) {
+      return 0;
+    }
+
+    const id = await Notifications.scheduleLocalNotificationAsync({
+      title: notification.message,
+      body: notification.message,
+      data: {
+        message: notification.message,
+        link: notification.link,
+      },
+      android: {
+        channelId: 'default',
+      },
+    }, {
+      time: time,
+    });
+
+    return id;
+  } catch (err) {
+    Sentry.captureException(err);
   }
-
-  const time = PNHelpers.notificationTime(notification.time);
-  if (isPast(time)) {
-    return 0;
-  }
-
-  const id = await Notifications.scheduleLocalNotificationAsync({
-    title: notification.message,
-    body: notification.message,
-    data: {
-      message: notification.message,
-      link: notification.link,
-    },
-    android: {
-      channelId: 'default',
-    },
-  }, {
-    time: time,
-  });
-
-  return id;
 }
 
 export async function cancelNotification(id) {
-  await Notifications.cancelScheduledNotificationAsync(id);
+  try {
+    await Notifications.cancelScheduledNotificationAsync(id);
+  } catch (err) {
+    Sentry.captureException(err);
+  }
 }
 
 export async function cancelAllNotifications() {
-  await Notifications.cancelAllScheduledNotificationsAsync();
+  try {
+    await Notifications.cancelAllScheduledNotificationsAsync();
+  } catch (err) {
+    Sentry.captureException(err);
+  }
 }
 
 export default {
